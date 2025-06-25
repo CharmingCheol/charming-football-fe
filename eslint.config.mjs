36233c2dc0ab85da2c1ba9f084e6d9955372267b
CharmingCheol <toby0806@naver.com>
2025-06-25 22:05:30 +0900

    npx storybook init 시 라이브러리 설치

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -8,5 +11,8 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended")];
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"),
+    ...storybook.configs["flat/recommended"],
+];
 export default eslintConfig;
