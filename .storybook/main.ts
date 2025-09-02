c2e580889cf3fb3751961b8558330c74ac939084
CharmingCheol <toby0806@naver.com>
2025-09-02 22:13:49 +0900

    스토리북 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,11 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-import path from "path";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: ["@chromatic-com/storybook", "@storybook/addon-essentials", "@storybook/addon-docs"],
+    addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-a11y", "@storybook/addon-vitest"],
     framework: {
-        name: "@storybook/nextjs",
+        name: "@storybook/react-vite",
         options: {},
-    },
-    staticDirs: ["../public"],
-    webpackFinal: (config) => {
-        if (config.resolve?.alias) {
-            config.resolve.alias["@"] = path.resolve(__dirname, "../src");
-        }
-        return config;
     },
 };
 export default config;
