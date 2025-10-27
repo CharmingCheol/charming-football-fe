c9b437ca1837c41af881720a58f176113b89cc55
CharmingCheol <toby0806@naver.com>
2025-10-27 21:24:14 +0900

    getSearchAllApi 객체 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,8 @@
 import { http, HttpResponse } from "msw";
+import { getSearchAllApi } from "@/apis/search";
 
-export const handlers = [];
+export const handlers = [
+    http.get(getSearchAllApi.path, () => {
+        return HttpResponse.json<Awaited<ReturnType<typeof getSearchAllApi.get>>>([]);
+    }),
+];
