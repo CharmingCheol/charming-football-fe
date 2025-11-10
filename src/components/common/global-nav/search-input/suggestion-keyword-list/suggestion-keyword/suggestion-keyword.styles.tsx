1805be15283c743c633a16e3f58accfd8b7f99b6
CharmingCheol <toby0806@naver.com>
2025-11-10 22:04:04 +0900

    selected props 전달해서 style 적용

--- src/components/common/global-nav/search-input/suggestion-keyword-list/suggestion-keyword/suggestion-keyword.styles.tsx
+++ src/components/common/global-nav/search-input/suggestion-keyword-list/suggestion-keyword/suggestion-keyword.styles.tsx
@@ -1,18 +1,15 @@
 import styled from "@emotion/styled";
 import { calcVh, colors, typography } from "@/styles";
 
-export const SuggestionKeyword = styled.li({
+export const SuggestionKeyword = styled.li<{ selected: boolean }>((props) => ({
     padding: `${calcVh(12)} ${calcVh(16)}`,
     cursor: "pointer",
     borderBottom: `1px solid ${colors.gray200}`,
     borderRadius: calcVh(8),
+    backgroundColor: props.selected ? colors.gray200 : "transparent",
 
     "&:last-child": {
         borderBottom: "none",
-    },
-
-    "&:hover": {
-        backgroundColor: colors.gray200,
     },
 
     "& .content": {
@@ -20,7 +17,7 @@ export const SuggestionKeyword = styled.li({
         justifyContent: "space-between",
         alignItems: "center",
     },
-});
+}));
 
 export const HighlightedText = styled.span({
     ...typography.p4,
