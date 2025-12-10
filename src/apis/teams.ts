3489ba8b3e35d2656589122dd558ceccbe137d74
CharmingCheol <toby0806@naver.com>
2025-12-10 20:49:30 +0900

    getNextMatchApi 추가

---
+++ src/apis/teams.ts
@@ -0,0 +1,10 @@
+import { apiClient } from "./instance";
+
+export const getNextMatchApi = {
+    path: "api/teams/:teamId/matches/next",
+    get: async (teamId: number): Promise<NextMatchData> => {
+        const path = getNextMatchApi.path.replace(":teamId", teamId.toString());
+        const response = await apiClient.get(path);
+        return response.data;
+    },
+};
