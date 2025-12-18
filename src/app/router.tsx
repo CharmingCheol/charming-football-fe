7955249a98c91efceb4c31d30c3bed28b2bc230d
CharmingCheol <toby0806@naver.com>
2025-12-18 21:26:56 +0900

    team 라우터 페이지 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "@/pages/main";
+import TeamPage from "@/pages/team/team";
 import Layout from "./layouts/layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
-        children: [{ path: "/", element: <MainPage /> }],
+        children: [
+            { path: "/", element: <MainPage /> },
+            { path: "/team/:id", element: <TeamPage /> },
+        ],
     },
 ]);
 
