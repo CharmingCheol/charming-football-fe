f5c185234c6178299ea9d5f4c989bb509d33b391
CharmingCheol <toby0806@naver.com>
2025-12-18 23:01:34 +0900

    match-overview-panel에 스토리북 추가

---
+++ src/pages/main/components/match-overview-panel/match-overview-panel.styles.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.section`
+    display: flex;
+    gap: 16px;
+`;
+
+export const NextMatchInfoWrapper = styled.div`
+    flex: 5;
+`;
+
+export const Div2 = styled.div`
+    flex: 2;
+`;
+
+export const Div3 = styled.div`
+    flex: 3;
+`;
--- src/pages/main/components/match-overview-panel/match-overview-panel.tsx
+++ src/pages/main/components/match-overview-panel/match-overview-panel.tsx
@@ -1,10 +1,15 @@
 import NextMatchInfo from "./next-match-info/next-match-info";
+import * as S from "./match-overview-panel.styles";
 
 const MatchOverviewPanel = () => {
     return (
-        <section>
-            <NextMatchInfo />
-        </section>
+        <S.Container>
+            <S.NextMatchInfoWrapper>
+                <NextMatchInfo />
+            </S.NextMatchInfoWrapper>
+            <S.Div2>2</S.Div2>
+            <S.Div3>3</S.Div3>
+        </S.Container>
     );
 };
 
