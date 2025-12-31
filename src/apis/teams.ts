72d2f0b3476903f6ca3715ebec7aeab85173d3f7
CharmingCheol <toby0806@naver.com>
2025-12-31 14:20:12 +0900

    getRecentMatchesApi 추가

--- src/apis/teams.ts
+++ src/apis/teams.ts
@@ -8,3 +8,14 @@ export const getNextMatchApi = {
         return response.data;
     },
 };
+
+export const getRecentMatchesApi = {
+    path: "api/teams/:teamId/matches/recent",
+    get: async (teamId: number): Promise<RecentMatchData[]> => {
+        const path = getRecentMatchesApi.path.replace(":teamId", teamId.toString());
+        const response = await apiClient.get(path, {
+            params: { last: 5 },
+        });
+        return response.data;
+    },
+};
