57e133bb841941ffdf70a3303e7a6e4aa812f632
CharmingCheol <toby0806@naver.com>
2026-02-11 22:48:27 +0900

    recent-match-result 내 react-query 적용

--- src/pages/main/components/match-overview-panel/recent-match-result/recent-match-result.stories.tsx
+++ src/pages/main/components/match-overview-panel/recent-match-result/recent-match-result.stories.tsx
@@ -1,14 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http, HttpResponse } from "msw";
-import { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
 import { getRecentMatchesApi } from "@/apis/teams";
+import { MANCHESTER_UNITED } from "@/constants/team";
 import Builder from "@/test/builder";
-import useMatchOverviewPanelStore, { initState } from "../match-overview-panel.store";
 import RecentMatchResult from "./recent-match-result";
 import Skeleton from "./skeleton/skeleton";
-import ErrorState from "./error-state/error-state";
-import { MANCHESTER_UNITED } from "@/constants/team";
 
 const meta: Meta<typeof RecentMatchResult> = {
     title: "pages/main/match-overview-panel/recent-match-result",
@@ -18,15 +15,19 @@ const meta: Meta<typeof RecentMatchResult> = {
     },
     decorators: [
         (Story) => {
-            useEffect(() => {
-                useMatchOverviewPanelStore.setState(initState);
-            }, []);
+            const queryClient = new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        retry: false,
+                    },
+                },
+            });
             return (
-                <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
                     <div style={{ width: "min(700px, 90vw)" }}>
                         <Story />
                     </div>
-                </BrowserRouter>
+                </QueryClientProvider>
             );
         },
     ],
@@ -136,5 +137,14 @@ export const 로딩중: Story = {
 };
 
 export const API_에러 = {
-    render: () => <ErrorState />,
+    parameters: {
+        msw: {
+            handlers: [
+                http.get(`*/${getRecentMatchesApi.path}`, () => {
+                    return HttpResponse.error();
+                }),
+            ],
+        },
+    },
+    render: () => <RecentMatchResult />,
 };
