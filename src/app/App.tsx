e6dadca085c4f6af3df94722bd9d1ab2ea4e7207
CharmingCheol <toby0806@naver.com>
2026-02-11 21:22:01 +0900

    QueryClient 세팅

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import Toast from "@/components/common/toast/toast";
 import { ErrorProvider } from "./error-boundary";
 import router from "./router";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 const App = () => {
     return (
-        <ErrorProvider>
-            <RouterProvider router={router} />
-            <Toast />
-        </ErrorProvider>
+        <QueryClientProvider client={queryClient}>
+            <ErrorProvider>
+                <RouterProvider router={router} />
+                <Toast />
+            </ErrorProvider>
+        </QueryClientProvider>
     );
 };
 
