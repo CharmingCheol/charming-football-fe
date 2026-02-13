d420e4c0f3338c4b3441192f43052a1c2a9955da
CharmingCheol <toby0806@naver.com>
2026-02-13 23:31:58 +0900

    toast 컴포넌트 제거

--- src/app/error-boundary.tsx
+++ src/app/error-boundary.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 import { useEffect } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { HttpError } from "@/constants/errors";
-import { useToastStore } from "@/components/common/toast/toast.store";
 
 export const ErrorProvider = ({ children }: { children: ReactNode }) => {
     useEffect(() => {
@@ -14,11 +13,9 @@ export const ErrorProvider = ({ children }: { children: ReactNode }) => {
 
             const error = event.reason;
             if (error instanceof HttpError) {
-                useToastStore.getState().add(error.message);
                 return;
             }
             if (error instanceof Error) {
-                useToastStore.getState().add("예상치 못한 오류가 발생했습니다");
                 return;
             }
         };
